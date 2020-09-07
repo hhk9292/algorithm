730f2f4499c32e2e70098c2dcec5442ec6dff95a
hhk9292 <hhk9292@gmail.com>
2020-09-07 19:04:37 +0900

    edit 1.py | Sep 07, 2020

--- 2019KAKAOBLIND/1.py
+++ 2019KAKAOBLIND/1.py
@@ -24,5 +24,3 @@ def solution(record):
         answer.append(ans)
 
     return answer
-
-solution(["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"])
