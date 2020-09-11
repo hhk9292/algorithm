fa168fbb0f300399c128e0de059c39353c42ce3d
hhk9292 <hhk9292@gmail.com>
2020-09-11 23:46:27 +0900

    baek 1157 | Sep 11, 2020

--- BAEK/README.md
+++ BAEK/README.md
@@ -6,5 +6,13 @@
 
   - [풀이](baek_14500.py)
 
+
+
+
+### [백준](https://www.acmicpc.net/)
+
 - [1197 최소 스패닝 트리](https://www.acmicpc.net/problem/1197)
   - [풀이](baek_1197.py)
+
+- [1157 단어 공부](https://www.acmicpc.net/problem/1157)
+  - [풀이](baek_1157.py)
---
+++ BAEK/baek_1157.py
@@ -0,0 +1,26 @@
+# BAEK
+# 단어 공부
+# https://www.acmicpc.net/problem/1157
+
+word = input()
+counting = [0] * 26  # 알파벳은 26개
+for letter in word:
+    if letter >= 'a':
+        counting[ord(letter) - 97] += 1  # a 의 아스키 코드는 97
+    else:
+        counting[ord(letter) - 65] += 1  # A의 아스키 코드는 65
+
+max_num = 0
+idx = -1
+for i in range(26):
+    if counting[i] > max_num:
+        max_num = counting[i]
+        idx = i
+
+result = ''
+if counting.count(max_num) >= 2:
+    result = '?'
+else:
+    result = chr(idx + 65)
+
+print(result)
